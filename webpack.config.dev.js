aab9d9dfaefc541eddf06216194f42e186dc2075
Ignacio Segura <ignacio.segura@x-team.com>
2017-05-04 00:48:58 +0200

    Initial build, ready to use

---
+++ webpack.config.dev.js
@@ -0,0 +1,12 @@
+// Webpack config for development
+
+
+var config = {
+  environment: 'development',
+  outputDir: '/dev',
+  debug: true,
+  devTool: 'source-map',
+  plugins: [],
+}
+
+module.exports = config;
